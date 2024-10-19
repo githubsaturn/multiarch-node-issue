9e9cdce6d9a3cc87cb731e0fac1a3140d3450971
Kasra Bigdeli <kasra85@gmail.com>
2024-10-19 15:17:41 -0700

    Inited

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# multiarch-node-issue
+# Reproducible case - multiarch issue with node 20 docker image
+
+TLDR:
+
+Creating a **multiarch image** (using buildx) works fine if we're using Node 18, and it breaks when we switch to Node 20.
+
+Works:
+
+```
+FROM node:18-alpine
+
+RUN echo "Node 18"
+RUN node -v
+RUN npm -v
+RUN echo "Finished Node 18"
+```
+
+Does not work:
+
+```
+FROM node:20-alpine
+
+RUN echo "Node 20"
+RUN node -v
+RUN npm -v
+RUN echo "Finished Node 20"
+```
---
+++ build_project.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DOCKERFILE=$1
+
+echo "Building Docker image for $DOCKERFILE"
+
+sudo apt-get update && sudo apt-get install qemu-user-static
+
+# export DOCKER_CLI_EXPERIMENTAL=enabled
+# docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+docker run --rm --privileged tonistiigi/binfmt --install all
+docker buildx ls
+docker buildx rm mybuilder || echo "mybuilder not found"
+docker buildx create --name mybuilder
+docker buildx use mybuilder
+
+docker buildx build --platform linux/arm -t my-test-image -f $DOCKERFILE --load .
+# docker buildx build --platform linux/amd64,linux/arm64,linux/arm -t my-test-image -f $DOCKERFILE -- .
